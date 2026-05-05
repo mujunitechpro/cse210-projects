507e0d885304cda023bcd9fd0949a23b7efc4c24
mujunitechpro <gozbertbaraka9@gmail.com>
2026-05-05 04:45:07 +0300

    ex3magicnumber

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+        {
+
+            Console.Write("What is the magic number? ");
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            int guess = -1;
+            int guessCount = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            Console.WriteLine($"You guessed it in {guessCount} tries!");
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
